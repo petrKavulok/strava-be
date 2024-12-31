d1c783f7fa1671cbfaabf6242b90cba34d3f3213
Petr Kavulok <petr.kavulok@ackee.cz>
2024-12-31 12:24:41 +0100

    Enhance Airtable API integration by adding dynamic table name support in sports.ts

--- api/sports.ts
+++ api/sports.ts
@@ -3,6 +3,7 @@ import fetch from 'node-fetch';
 
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY!;
 const BASE_ID = process.env.AIRTABLE_BASE_ID!;
+const AIRTABLE_TABLE_NAME = process.env.AIRTABLE_TABLE_NAME!;
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   // Enable CORS
@@ -18,13 +19,13 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 
   try {
     const response = await fetch(
-      `https://api.airtable.com/v0/${BASE_ID}/SportsTable`,
-      {
-        headers: {
-          Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-        },
-      }
-    );
+        `https://api.airtable.com/v0/${BASE_ID}/${AIRTABLE_TABLE_NAME}?sort%5B0%5D%5Bfield%5D=weekNo&sort%5B0%5D%5Bdirection%5D=desc`,
+        {
+          headers: {
+            Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+          },
+        }
+      );
 
     if (!response.ok) {
       throw new Error('Failed to fetch data from Airtable');
